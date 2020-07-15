3babb9709382210d879a719b0747b2fa0cf44e79
Devin Leonhart <devinleonhart@fastmail.com>
2020-07-14 20:48:44 -0700

    Update script for emoticon generation.

--- emoticon-script/convert.js
+++ emoticon-script/convert.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.resolve(__dirname, 'input/input.txt'), 'utf8');
-const leftEmoticonTemplate = fs.readFileSync(path.resolve(__dirname, 'emoticon-left.mustache'), 'utf8');
-const rightEmoticonTemplate = fs.readFileSync(path.resolve(__dirname, 'emoticon-right.mustache'), 'utf8');
-const storyMustache = fs.readFileSync(path.resolve(__dirname, 'story.mustache'), 'utf8');
+const emoteTemplate = fs.readFileSync(path.resolve(__dirname, 'emote.mustache'), 'utf8');
 const lines = input.split('\n');
 const Mustache = require('mustache');
 
 let name, expression, dialog, view;
-let firstCharacter = null;
+
+let lastSpeaker = null;
 
 const parsedLines = lines.filter(line => line.length > 0).map((line) => {
   // Split character from dialog.
@@ -21,26 +20,31 @@ const parsedLines = lines.filter(line => line.length > 0).map((line) => {
   
   // Generate Mustache Template
   view = {
-    character_name: name,
-    character_name_class: name.toLowerCase(),
-    character_dialog: dialog,
-    character_expression: `${name.toLowerCase()}_${expression}`,
+    character: name.toLowerCase(),
+    emotion: expression,
+    side: determineDirection(name),
+    dialog: dialog,
+    name: name
   }
 
   return Mustache.render(
-    determineDirection(name) === 'left' ? leftEmoticonTemplate : rightEmoticonTemplate, view
+    emoteTemplate, view
   );
 });
 
-fs.writeFileSync(path.resolve(__dirname, './output/output.html'), Mustache.render(storyMustache, {dialog: parsedLines.join('\n')}), 'utf8');
+fs.writeFileSync(path.resolve(__dirname, './output/output.html'), parsedLines.join('\n'), 'utf8');
 
 function determineDirection(name) {
-  if(firstCharacter === null) {
-    firstCharacter = name;
+  if (lastSpeaker === null) {
+    lastSpeaker = {direction: 'left', name: name};
     return 'left';
   }
-  else if(firstCharacter === name) {
-    return 'left';
+  else if(lastSpeaker.name === name) {
+    return lastSpeaker.direction;
   }
-  return 'right';
+  else {
+    lastSpeaker.name = name;
+    lastSpeaker.direction = lastSpeaker.direction === 'left' ? 'right' : 'left'
+    return lastSpeaker.direction;
+  }
 }
